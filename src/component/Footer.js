4325b2680d3d358889c14dc1b6989032b286096f
trieunhuyenquoc <trieunguyenquoc020604@gmai.com>
2023-12-28 10:17:16 +0700

    xong

---
+++ src/component/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <div className={'row'}>
+            <div className="col-12">
+                <h1>Footer</h1>
+            </div>
+        </div>
+    )
+}
---
+++ src/component/Header.js
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+        <div className={'row'}>
+            <div className="col-12">
+            <h1>Header</h1>
+            </div>
+        </div>
+    )
+}
